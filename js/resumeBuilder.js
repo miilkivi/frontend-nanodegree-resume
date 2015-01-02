384a232ff4b5c149ad8d627c090437b9e474b1dc
miilkivi <mikakivi@Mikas-MacBook-Pro.local>
2015-01-02 15:38:51 +0200

    Start

---
+++ js/resumeBuilder.js
@@ -0,0 +1,110 @@
+
+var skills = ["JS", "Leadership", "Project Management"];
+
+
+var bio = {
+	"name" : "Mika",
+	"role" : "Front end developer",
+	"contactInfo" : {
+		"email" : "miilkivi@gmail.com",
+		"phone" : "+358 443615123",
+		"url" : "kivi.com",
+		"twitter" : "@mi_kivi",
+		"instagram" : "@miilkivi",
+		"linkedin" : "fi.linkedin.com/in/mikivi"
+
+	},
+	"pictureUrl" : "pictures/bio.jpg",
+	"welcomeMessage" : "I will be great",
+	"skills" : skills 
+}
+
+var work = {
+	"jobs" : [
+		{
+			"position" : "Warehouse worker",
+			"employer" : "Firecon Group Oy",
+			"years" : ["05/2008-12/2008", "05/2010-08/2010", "12/2010-11/2011", "08/2011-08/2014"],
+			"description" : "Warehouse work, pipes and valves prefabrication and pipe installations"
+		},
+		{
+			"position" : "Project assistant",
+			"employer" : "Kustom promotion",
+			"years" : "12/2013-04/2014"
+			"description" : "Working at Kustom Promotion for 3 different snowboarding events: Ruka Snowboard World Cup 2013, Burton Rail Days, ISOPEN 2014. Tasks: cooking, building of track and many other small tasks."
+		},
+		{
+			"position" : "Student feedback collector",
+			"employer" : "Univeristy of Jyväskylä, Department of Mathematical Information Technology",
+			"years"	 : "1/2011-12/2011"
+			"description" : "Collecting, analyzing course feedback at Department of Mathematical Information Technology and improwing the feedback collection process"
+		},
+	]
+}
+
+var education = {
+	"schools" : [
+		{
+			"name" : "University of Helsinki",
+			"city" : "Helsinki",
+			"years" : "09/2011-",
+			"degree" : "BS",
+			"major" : "Computer Science",
+			"minor" : ["Mathematics", "Economics"]
+		},
+		{
+			"name" : "Univeristy of Helsinki",	
+			"city" : "Helsinki",
+			"years" : "09/2013-",
+			"degree" : "M.Sc.",
+			"major" : "Computer Science",
+			"minor" : "Leadership"
+		},
+		{
+			"name" : "Polytechinc Universidad de Catalunya",
+			"city" : "Barcelona",
+			"years" : "09/2014-01/2015"
+			"degree" : "Exchange Student"
+			"major" : "Computer Science"
+		},
+		{
+			"name" : "Univeristy of Eastern Finland",
+			"city" : "Joensuu",
+			"years" : "09/2013-"
+			"degree" : "Minor Subject",
+			"minor" : "Economics"
+		},
+		{
+			"name" : "Univeristy of Jyväskylä",
+			"city" : "Jyväskylä",
+			"years" : ""
+			"degree" : "BS",
+			"major" : "Computer Science"
+		}
+	]
+,
+	"online courses" :[
+		{
+			"title" : "Javascript Basics",
+			"school" : "Udacity",
+			"dates" : 2014,
+			"url" : "https://www.udacity.com/course/ud804"
+		},
+		{
+			"title" : "Intro to HTML5 and CSS",
+			"school" : "Udacity",
+			"dates" : 2014,
+			"url" : "https://www.udacity.com/course/ud304"
+		},
+		{
+			"title" : "How to Build a Startup",
+			"school" : "Udacity",
+			"dates" : "2014-2015",
+			"url" : "https://www.udacity.com/course/ep245"
+		},
+	]
+}	
+
+
+
+
